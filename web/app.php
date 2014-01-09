4adec0e20453ecc747030a09e30062103be2a59d
fredriktid <fredrik.tid@gmail.com>
2014-01-09 15:06:40 -0500

    Enabled Symfonys reverse proxy

--- web/app.php
+++ web/app.php
@@ -20,7 +20,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
