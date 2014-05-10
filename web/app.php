62c020b6ed47f2fcd74f21cd3245ed405afd3113
Fredrik Tidemann <fredrik.tid@gmail.com>
2014-05-10 18:20:37 +0200

    umask to 002

--- web/app.php
+++ web/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-umask(0000);
+umask(002);
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 //$loader = new ApcClassLoader('codekeepr', $loader);
