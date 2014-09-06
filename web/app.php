873ddff0b8100dfcd3f499fba676fb37a952d6c2
Fredrik Tidemann <fredrik@netmaking.no>
2014-09-06 20:48:42 +0200

    Use the Symfony reverse proxy with ESI

--- web/app.php
+++ web/app.php
@@ -5,19 +5,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 umask(002);
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-//$loader = new ApcClassLoader('codekeepr', $loader);
-//$loader->register(true);
-
+require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-Request::enableHttpMethodParameterOverride();
+// wrap the default AppKernel with the AppCache one
+$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
-
