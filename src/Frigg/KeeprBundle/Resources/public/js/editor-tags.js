83054811749a5f273b915ae4df97e0b837b8b4af
Fredrik Tidemann <fredrik@netmaking.no>
2014-11-30 01:42:26 +0000

    Adds tags plugin

---
+++ src/Frigg/KeeprBundle/Resources/public/js/editor-tags.js
@@ -0,0 +1,66 @@
+(function ($) {
+
+    $.fn.editorTags = function(options) {
+
+        var context = this;
+
+        this.editor = false;
+        this.tags = false;
+
+        this.init = function (editor) {
+
+            this.editor = editor;
+            this.tags = $(this);
+
+            var $add = $('<a href="#" class="add_tag_link margin-bottom btn"><i class="icon-tag"></i> Add tag</a>');
+            var $new = $('<li></li>').append($add);
+
+            $(this).find('li').each(function () {
+                context.addDelete($(this));
+            });
+
+            $(this).prepend($new);
+
+            $(this).data('index', $(this).find(':input').length);
+
+            $add.on('click', function (e) {
+                e.preventDefault();
+                context.addForm(context.tags, $new);
+            });
+        }
+
+        this.addForm = function($button, $new) {
+
+            var prototype = $button.data('prototype');
+            var index = $button.data('index');
+
+            // Replace '__name__' in the prototype's HTML to
+            // instead be a number based on how many items we have
+            var newForm = prototype.replace(/__name__/g, index);
+
+            $button.data('index', index + 1);
+
+            var $newFormLi = $('<li></li>').append(newForm);
+
+            $new.after($newFormLi);
+
+            context.editor.searcher();
+
+            this.addDelete($newFormLi);
+        }
+
+        this.addDelete = function($form) {
+            var $deleteForm = $('<a href="#" class="btn btn-danger btn-tag-delete"><i class="icon-trash icon-white"></i></a>');
+            $form.prepend($deleteForm);
+
+            $deleteForm.on('click', function(e) {
+                e.preventDefault();
+                $form.remove();
+            });
+        }
+
+        this.data('context', this);
+
+        return this;
+    }
+}(jQuery));
