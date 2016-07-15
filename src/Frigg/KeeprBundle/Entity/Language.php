bfb71ffa016eb4a72fc706241e464c69c139c9e8
Fredrik Tidemann <fredrik@netmaking.no>
2016-07-15 17:48:21 +0200

    Nullable identifier columns

--- src/Frigg/KeeprBundle/Entity/Language.php
+++ src/Frigg/KeeprBundle/Entity/Language.php
@@ -24,7 +24,7 @@ class Language implements SanitizableIdentifierInterface
     private $name;
 
     /**
-     * @ORM\Column(type="string", unique=true, length=255)
+     * @ORM\Column(type="string", unique=true, length=255, nullable=true)
      */
     private $identifier;
 
