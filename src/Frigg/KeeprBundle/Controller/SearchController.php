12d679ca2ba26e98cbe56b4ca3d76b76c3973179
Fredrik Tidemann <fredrik.tid@gmail.com>
2016-07-24 18:59:19 +0000

    Fixes posts by tag from Elastica

--- src/Frigg/KeeprBundle/Controller/SearchController.php
+++ src/Frigg/KeeprBundle/Controller/SearchController.php
@@ -3,10 +3,8 @@
 namespace Frigg\KeeprBundle\Controller;
 
 use Elastica\Filter\Range;
-use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use Frigg\KeeprBundle\Entity\Tag;
 use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
-use Knp\Component\Pager\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -57,16 +55,16 @@ class SearchController extends Controller
     /**
      * Search and show list.
      *
-     * @Route("/list/{type}", name="search_list", defaults={"type" = "post"})
+     * @Route("/posts", name="search_posts")
      * @Method("GET")
-     * @Template("FriggKeeprBundle:Search:list.html.twig")
+     * @Template("FriggKeeprBundle:Search:posts.html.twig")
      */
-    public function listAction($type)
+    public function postsAction()
     {
         $queryText = $this->get('request')->query->get('query', '');
         $currentPage = $this->get('request')->query->get('page', 1);
         $pageLimit = $this->getParameter('codekeepr.page.limit');
-
+        
         $queryString = new Query\QueryString();
         $queryString->setQuery(sprintf('*%s*', $queryText));
 
@@ -75,7 +73,7 @@ class SearchController extends Controller
             ->setQuery($queryString)
             ->setSize(99999);
 
-        $entries = $this->get(sprintf('fos_elastica.finder.website.%s', $type))
+        $entries = $this->get('fos_elastica.finder.website.post')
             ->find($query);
 
         /** @var SlidingPagination $pager */
@@ -97,21 +95,21 @@ class SearchController extends Controller
     /**
      * Posts by date
      *
-     * @Route("/date/{dateString}", name="search_date")
+     * @Route("/date", name="search_date")
      * @Method("GET")
      * @Template("FriggKeeprBundle:Search:view.html.twig")
      */
-    public function dateAction($dateString)
+    public function dateAction()
     {
-        $dateTs = strtotime($dateString);
-        $dateFormat = date('Y-m-d', $dateTs);
-
-        $queryText = $this->get('request')->query->get('query', '*');
+        $query = $this->get('request')->query->get('query', 0);
         $currentPage = $this->get('request')->query->get('page', 1);
         $pageLimit = $this->getParameter('codekeepr.page.limit');
 
+        $dateTs = strtotime($query);
+        $dateFormat = date('Y-m-d', $dateTs);
+
         $queryString = new Query\QueryString();
-        $queryString->setQuery($queryText);
+        $queryString->setQuery('*');
 
         $rangeLower = new Query\Filtered($queryString, new Range('created_at', [
             'gte' => $dateFormat
@@ -139,8 +137,7 @@ class SearchController extends Controller
         );
 
         $pager->setUsedRoute('search_date');
-        $pager->setParam('dateString', $dateString);
-        $pager->setParam('query', $queryText);
+        $pager->setParam('query', $query);
         $pager->setParam('page', $currentPage);
 
         return [
@@ -152,16 +149,17 @@ class SearchController extends Controller
     /**
      * Posts by tag
      *
-     * @Route("/tag/{tag}", name="search_tag")
+     * @Route("/tag", name="search_tag")
      * @Method("GET")
      * @Template("FriggKeeprBundle:Search:view.html.twig")
      */
-    public function tagAction($tag)
+    public function tagAction()
     {
+        $query = $this->get('request')->query->get('query', 0);
         $em = $this->get('doctrine.orm.entity_manager');
 
         /** @var Tag $tagEntity */
-        if (!$tagEntity = $em->getRepository('FriggKeeprBundle:Tag')->findOneByIdentifier($tag)) {
+        if (!$tagEntity = $em->getRepository('FriggKeeprBundle:Tag')->findOneByIdentifier($query)) {
             throw $this->createNotFoundException(
                 $this->get('translator')->trans('Unable to find tag')
             );
@@ -170,14 +168,22 @@ class SearchController extends Controller
         $currentPage = $this->get('request')->query->get('page', 1);
         $pageLimit = $this->getParameter('codekeepr.page.limit');
 
-        $tagsQuery = new Query\Terms();
-        $tagsQuery->setTerms('Tags', [$tagEntity->getId()]);
+        $matchQuery = new Query\Match();
+        $matchQuery->setField('Tags.name', $tagEntity->getName());
 
         $boolQuery = new Query\BoolQuery();
-        $boolQuery->addShould($tagsQuery);
+        $boolQuery->addMust($matchQuery);
+
+        $nestedQuery = new Query\Nested();
+        $nestedQuery->setPath('Tags');
+        $nestedQuery->setQuery($boolQuery);
+
+        $query = new Query($nestedQuery);
+        $query->setSize(99999);
+        $query->setSort(['created_at' => ['order' => 'desc']]);
 
         $entries = $this->get('fos_elastica.finder.website.post')
-            ->find($boolQuery);
+            ->find($query);
 
         /** @var SlidingPagination $pager */
         $pager = $this->get('knp_paginator')->paginate(
@@ -186,12 +192,12 @@ class SearchController extends Controller
             $pageLimit
         );
 
-        $pager->setUsedRoute('search_date');
-        $pager->setParam('tag', $tag);
+        $pager->setUsedRoute('search_tag');
+        $pager->setParam('query', $query);
         $pager->setParam('page', $currentPage);
 
         return [
-            'title' => $tagEntity->getIdentifier(),
+            'title' => $tagEntity->getName(),
             'entries' => $pager
         ];
     }
@@ -199,17 +205,17 @@ class SearchController extends Controller
     /**
      * jQuery Autocomplete
      *
-     * @Route("/autocomplete/{type}", name="search_autocomplete", defaults={"type" = "post"})
+     * @Route("/autocomplete", name="search_autocomplete")
      * @Method("GET")
      */
-    public function autocompleteAction($type)
+    public function autocompleteAction()
     {
         $query = $this->get('request')->query->get('query', '');
         $method = $this->get('request')->query->get('method', 'json');
 
         $collection = array();
         if ($query) {
-            $finder = $this->get('fos_elastica.finder.website.'.$type);
+            $finder = $this->get('fos_elastica.finder.website.post');
             $results = $finder->find($query.'*', 5);
             foreach ($results as $object) {
                 $collection[] = array(
